f12ed472f75d3393326a02776bb88f8f9604758a
amalygin <jalex2006@gmail.com>
2019-02-05 00:55:32 +0300

    fix wrapper

--- src/Zio/FileSystems/FileSystemWrapper.cs
+++ src/Zio/FileSystems/FileSystemWrapper.cs
@@ -29,122 +29,122 @@ namespace Zio.FileSystems {
         #region IFileSystem members
 
         /// <inheritdoc />
-        public void CreateDirectory(UPath path) {
+        public virtual void CreateDirectory(UPath path) {
             OrigFileSystem.CreateDirectory(path);
         }
 
         /// <inheritdoc />
-        public bool DirectoryExists(UPath path) {
+        public virtual bool DirectoryExists(UPath path) {
             return OrigFileSystem.DirectoryExists(path);
         }
 
         /// <inheritdoc />
-        public void MoveDirectory(UPath srcPath, UPath destPath) {
+        public virtual void MoveDirectory(UPath srcPath, UPath destPath) {
             OrigFileSystem.MoveDirectory(srcPath, destPath);
         }
 
         /// <inheritdoc />
-        public void DeleteDirectory(UPath path, bool isRecursive) {
+        public virtual void DeleteDirectory(UPath path, bool isRecursive) {
             OrigFileSystem.DeleteDirectory(path, isRecursive);
         }
 
         /// <inheritdoc />
-        public void CopyFile(UPath srcPath, UPath destPath, bool overwrite) {
+        public virtual void CopyFile(UPath srcPath, UPath destPath, bool overwrite) {
             OrigFileSystem.CopyFile(srcPath, destPath, overwrite);
         }
 
         /// <inheritdoc />
-        public void ReplaceFile(UPath srcPath, UPath destPath, UPath destBackupPath, bool ignoreMetadataErrors) {
+        public virtual void ReplaceFile(UPath srcPath, UPath destPath, UPath destBackupPath, bool ignoreMetadataErrors) {
             OrigFileSystem.ReplaceFile(srcPath, destPath, destBackupPath, ignoreMetadataErrors);
         }
 
         /// <inheritdoc />
-        public long GetFileLength(UPath path) {
+        public virtual long GetFileLength(UPath path) {
             return OrigFileSystem.GetFileLength(path);
         }
 
         /// <inheritdoc />
-        public bool FileExists(UPath path) {
+        public virtual bool FileExists(UPath path) {
             return OrigFileSystem.FileExists(path);
         }
 
         /// <inheritdoc />
-        public void MoveFile(UPath srcPath, UPath destPath) {
+        public virtual void MoveFile(UPath srcPath, UPath destPath) {
             OrigFileSystem.MoveFile(srcPath, destPath);
         }
 
         /// <inheritdoc />
-        public void DeleteFile(UPath path) {
+        public virtual void DeleteFile(UPath path) {
             OrigFileSystem.DeleteFile(path);
         }
 
         /// <inheritdoc />
-        public Stream OpenFile(UPath path, FileMode mode, FileAccess access, FileShare share = FileShare.None) {
+        public virtual Stream OpenFile(UPath path, FileMode mode, FileAccess access, FileShare share = FileShare.None) {
             return OrigFileSystem.OpenFile(path, mode, access, share);
         }
 
         /// <inheritdoc />
-        public FileAttributes GetAttributes(UPath path) {
+        public virtual FileAttributes GetAttributes(UPath path) {
             return OrigFileSystem.GetAttributes(path);
         }
 
         /// <inheritdoc />
-        public void SetAttributes(UPath path, FileAttributes attributes) {
+        public virtual void SetAttributes(UPath path, FileAttributes attributes) {
             OrigFileSystem.SetAttributes(path, attributes);
         }
 
         /// <inheritdoc />
-        public DateTime GetCreationTime(UPath path) {
+        public virtual DateTime GetCreationTime(UPath path) {
             return OrigFileSystem.GetCreationTime(path);
         }
 
         /// <inheritdoc />
-        public void SetCreationTime(UPath path, DateTime time) {
+        public virtual void SetCreationTime(UPath path, DateTime time) {
             OrigFileSystem.SetCreationTime(path, time);
         }
 
         /// <inheritdoc />
-        public DateTime GetLastAccessTime(UPath path) {
+        public virtual DateTime GetLastAccessTime(UPath path) {
             return OrigFileSystem.GetLastAccessTime(path);
         }
 
         /// <inheritdoc />
-        public void SetLastAccessTime(UPath path, DateTime time) {
+        public virtual void SetLastAccessTime(UPath path, DateTime time) {
             OrigFileSystem.SetLastAccessTime(path, time);
         }
 
         /// <inheritdoc />
-        public DateTime GetLastWriteTime(UPath path) {
+        public virtual DateTime GetLastWriteTime(UPath path) {
             return OrigFileSystem.GetLastWriteTime(path);
         }
 
         /// <inheritdoc />
-        public void SetLastWriteTime(UPath path, DateTime time) {
+        public virtual void SetLastWriteTime(UPath path, DateTime time) {
             OrigFileSystem.SetLastWriteTime(path, time);
         }
 
         /// <inheritdoc />
-        public IEnumerable<UPath> EnumeratePaths(UPath path, string searchPattern, SearchOption searchOption, SearchTarget searchTarget) {
+        public virtual IEnumerable<UPath> EnumeratePaths(UPath path, string searchPattern, SearchOption searchOption, SearchTarget searchTarget) {
             return OrigFileSystem.EnumeratePaths(path, searchPattern, searchOption, searchTarget);
         }
 
         /// <inheritdoc />
-        public bool CanWatch(UPath path) {
+        public virtual bool CanWatch(UPath path) {
             return OrigFileSystem.CanWatch(path);
         }
 
         /// <inheritdoc />
-        public IFileSystemWatcher Watch(UPath path) {
+        public virtual IFileSystemWatcher Watch(UPath path) {
             return OrigFileSystem.Watch(path);
         }
 
         /// <inheritdoc />
-        public string ConvertPathToInternal(UPath path) {
+        public virtual string ConvertPathToInternal(UPath path) {
             return OrigFileSystem.ConvertPathToInternal(path);
         }
 
         /// <inheritdoc />
-        public UPath ConvertPathFromInternal(string systemPath) {
+        public virtual UPath ConvertPathFromInternal(string systemPath) {
             return OrigFileSystem.ConvertPathFromInternal(systemPath);
         }
 
@@ -153,7 +153,7 @@ namespace Zio.FileSystems {
         #region IDisposable members
 
         /// <inheritdoc />
-        public void Dispose() {
+        public virtual void Dispose() {
             if(_owned) {
                 OrigFileSystem.Dispose();
             }
